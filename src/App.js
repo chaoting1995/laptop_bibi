b83a9e5043ba808edcd2343d9a733e8cae5404c3
chaoting <02115214@gm.scu.edu.tw>
2020-11-22 16:19:51 +0800

    20201122-16:18 can yarn start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// 引入 共用元件
+// import Navbar from 'Share/Components/NavBar/NavBar';
+// import Footer from 'Share/Components/Footer/Footer';
+//加入 ScrollToTop
+// import ScrollToTop from 'Share/Components/ScrollToTop/ScrollToTop';
 
+// 引入 所有人的總元件
+// import IrisUserprofile from 'Iris/Pages/IrisUserprofile';
+
+// 安裝react router => npm install react-router-dom
+// 引入react router => 用於制定路由
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// 路由表
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>元件一定要放在最外層
+    <Router>
+      <>
+        {/* 放切頁時不重新渲染的部份 s*/}
+        {/* <Navbar/> */}
+        {/* 放切頁時不重新渲染的部份 e*/}
+        {/* 路由設定開始 */}
+
+        {/* <ScrollToTop> */}
+        <Switch>
+          {/* 放"page資料夾"內的元件 */}
+          {/* <Route exact path="/"> */}
+          {/* <ClaudiaFarmIndex /> */}
+          {/* </Route> */}
+        </Switch>
+        {/* </ScrollToTop>
+        路由設定結束 */}
+
+        {/* 放切頁時不重新渲染的部份 s*/}
+        {/* <Footer /> */}
+        {/* 放切頁時不重新渲染的部份 e*/}
+      </>
+    </Router>
   );
 }
 
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+function HomePage(props) {
+  const [count, setCount] = useState(1);
+  return <></>;
+}
+export default HomePage;
