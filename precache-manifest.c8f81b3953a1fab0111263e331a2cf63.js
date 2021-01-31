6c4249b6fc3ceb4013874a910f5a9edc0445a012
chaoting <02115214@gm.scu.edu.tw>
2021-01-31 15:53:26 +0800

    Updates

---
+++ precache-manifest.c8f81b3953a1fab0111263e331a2cf63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbee0dd4aa8d3b48daffdb0fe031e36",
+    "url": "/laptop_bibi/index.html"
+  },
+  {
+    "revision": "0ac2580a5afdb7bf1845",
+    "url": "/laptop_bibi/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "536df11a243af7e0332b",
+    "url": "/laptop_bibi/static/css/main.d2fa89ff.chunk.css"
+  },
+  {
+    "revision": "0ac2580a5afdb7bf1845",
+    "url": "/laptop_bibi/static/js/2.ff78bae3.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/laptop_bibi/static/js/2.ff78bae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536df11a243af7e0332b",
+    "url": "/laptop_bibi/static/js/main.c3fc327f.chunk.js"
+  },
+  {
+    "revision": "ec204a5c128495eb9c97",
+    "url": "/laptop_bibi/static/js/runtime-main.caaccf63.js"
+  },
+  {
+    "revision": "774d50c7c207e8655da0484b2584a510",
+    "url": "/laptop_bibi/static/media/logo.774d50c7.svg"
+  },
+  {
+    "revision": "ae637fb45bedb8436ecdce4f2c7b6f71",
+    "url": "/laptop_bibi/static/media/search_icon.ae637fb4.svg"
+  }
+]);
